52c6aabf814fc40875b119edf7808d2b0b14062e
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2021-01-05 09:38:42 +0000

    bumping minor version

--- 03_01-StockRank/StockRank/AppDelegate.swift
+++ 03_01-StockRank/StockRank/AppDelegate.swift
@@ -13,4 +13,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Overri
+        // Override point for custo

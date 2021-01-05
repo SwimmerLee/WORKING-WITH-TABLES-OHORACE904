cf2c832940f0fd9a022af31be6f5ea3aad50a537
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2021-01-05 15:18:18 +0000

    devfreq: Use high priority workqueue

--- 03_01-StockRank/StockRank/AppDelegate.swift
+++ 03_01-StockRank/StockRank/AppDelegate.swift
@@ -13,4 +13,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after applica
+        // Override point for customization after application launch.
+       

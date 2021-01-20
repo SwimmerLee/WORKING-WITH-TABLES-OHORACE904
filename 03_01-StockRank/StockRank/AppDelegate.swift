da6e27e8191e5f7fddbd325113c5423a1b25a1a2
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2021-01-20 08:33:11 +0000

    menu updates

--- 03_01-StockRank/StockRank/AppDelegate.swift
+++ 03_01-StockRank/StockRank/AppDelegate.swift
@@ -20,4 +20,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is
+        // Called when a new scene session is being created.
+    

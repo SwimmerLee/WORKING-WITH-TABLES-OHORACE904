adb9e878c391327baa20f9a4e12dd6ceb3a497e3
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2021-02-19 05:47:35 +0000

    Btrfs: don't delay inode ref updates during log replay

--- 03_01-StockRank/StockRank/AppDelegate.swift
+++ 03_01-StockRank/StockRank/AppDelegate.swift
@@ -25,4 +25,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSes
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneS

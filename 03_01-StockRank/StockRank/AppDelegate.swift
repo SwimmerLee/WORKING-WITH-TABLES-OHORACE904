aa33bc1e5b246b32ff66a7b9aa32bb4c56395f2e
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2021-02-23 14:01:21 +0000

    homepage

--- 03_01-StockRank/StockRank/AppDelegate.swift
+++ 03_01-StockRank/StockRank/AppDelegate.swift
@@ -26,4 +26,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        /
+        // Called when the use

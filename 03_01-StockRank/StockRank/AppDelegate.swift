b9ca728c092dae38df78101d0a5da972f5930b2e
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2021-03-03 07:11:06 +0000

    save

--- 03_01-StockRank/StockRank/AppDelegate.swift
+++ 03_01-StockRank/StockRank/AppDelegate.swift
@@ -27,4 +27,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
-        // If 
+        // If any sessions were

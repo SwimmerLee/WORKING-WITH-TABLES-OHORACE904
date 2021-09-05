36b1b07dd80741849d8b217511636519ed962b18
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2021-09-05 20:18:22 +0000

    3.3

--- 03_01-StockRank/StockRank/SceneDelegate.swift
+++ 03_01-StockRank/StockRank/SceneDelegate.swift
@@ -12,4 +12,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
 
-    func scene(_ scene: UIScene, w
+    func scene(_ scene: UIScene, willConnectTo session: UISc

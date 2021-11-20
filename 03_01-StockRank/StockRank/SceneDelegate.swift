3a1f62656505dee975072a9722bcc12294195201
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2021-11-20 23:08:31 +0000

    Label

--- 03_01-StockRank/StockRank/SceneDelegate.swift
+++ 03_01-StockRank/StockRank/SceneDelegate.swift
@@ -21,4 +21,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
-        // This occ
+        // This occurs shortly after the s

2a7bdd1a371ef1305ed47be58033cacb79bbe8ac
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2022-08-11 18:26:49 +0000

    Update SetupComplete.cmd

--- 03_01-StockRank/StockRank/StockRankCollectionViewCell.swift
+++ 03_01-StockRank/StockRank/StockRankCollectionViewCell.swift
@@ -28,4 +28,4 @@ class StockRankCollectionViewCell: UICollectionViewCell {
     //데이터를 업데이트하는 매소드
     func configure(_ stock: StockModel) {
         rankLabel.text = "\(stock.rank)"
-        compan
+        companyIconImageView.imag

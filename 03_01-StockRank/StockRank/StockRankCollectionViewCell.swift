4d033a2b46e7f0f221d310bc0d26c8e44439aa16
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2022-10-01 07:25:19 +0000

    code refine

--- 03_01-StockRank/StockRank/StockRankCollectionViewCell.swift
+++ 03_01-StockRank/StockRank/StockRankCollectionViewCell.swift
@@ -50,4 +50,4 @@ class StockRankCollectionViewCell: UICollectionViewCell {
         let numberFormatter = NumberFormatter()
         numberFormatter.numberStyle = .decimal
         numberFormatter.maximumFractionDigits = 0
-        let result = num
+        let result = numberFormatter.s

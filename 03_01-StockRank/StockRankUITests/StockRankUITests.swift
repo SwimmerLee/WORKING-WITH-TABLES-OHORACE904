4cf1f148ade45b80b39448bcf71429d422999444
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2022-12-30 12:39:44 +0000

    Another test.

--- 03_01-StockRank/StockRankUITests/StockRankUITests.swift
+++ 03_01-StockRank/StockRankUITests/StockRankUITests.swift
@@ -10,4 +10,6 @@ import XCTest
 class StockRankUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test metho
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    

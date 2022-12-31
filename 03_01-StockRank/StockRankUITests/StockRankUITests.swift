ebebd45b9d3ad6e5c14f3ece8a4e7fe330a9786d
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2022-12-31 17:01:50 +0000

    test js

--- 03_01-StockRank/StockRankUITests/StockRankUITests.swift
+++ 03_01-StockRank/StockRankUITests/StockRankUITests.swift
@@ -12,4 +12,4 @@ class StockRankUITests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
-        // In UI tests it is 
+        // In UI tests it is usually best to st

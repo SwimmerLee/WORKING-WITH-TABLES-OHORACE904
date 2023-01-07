cead46df7e6993368e9db127523a8a485978635c
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2023-01-07 14:54:10 +0000

    ALSA: hda - Fix missing beep on ASUS X43U notebook

--- 03_01-StockRank/StockRankUITests/StockRankUITests.swift
+++ 03_01-StockRank/StockRankUITests/StockRankUITests.swift
@@ -15,4 +15,4 @@ class StockRankUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The 
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good

d06233561409c45e61a7e20b098c7dc2ef9d6084
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2023-01-08 13:18:38 +0000

    mingw: be more defensive when making the environment block

--- 03_01-StockRank/StockRankUITests/StockRankUITests.swift
+++ 03_01-StockRank/StockRankUITests/StockRankUITests.swift
@@ -16,4 +16,6 @@ class StockRankUITests: XCTestCase {
         continueAfterFailure = false
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-  
+    }
+
+    overri

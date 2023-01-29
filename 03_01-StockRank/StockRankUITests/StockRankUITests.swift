a1a536941ccca4ab6f41ca7510a7d60a76ec6408
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2023-01-29 15:49:09 +0000

    BugID: 270

--- 03_01-StockRank/StockRankUITests/StockRankUITests.swift
+++ 03_01-StockRank/StockRankUITests/StockRankUITests.swift
@@ -22,4 +22,5 @@ class StockRankUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func t
+    func testExample() throws {
+    

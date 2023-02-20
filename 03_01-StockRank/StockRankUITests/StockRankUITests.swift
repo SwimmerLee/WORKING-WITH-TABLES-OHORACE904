67a50d90e8287d9e8819826eb1e1de9092b92b48
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2023-02-20 14:54:09 +0000

    profiler

--- 03_01-StockRank/StockRankUITests/StockRankUITests.swift
+++ 03_01-StockRank/StockRankUITests/StockRankUITests.swift
@@ -31,4 +31,5 @@ class StockRankUITests: XCTestCase {
     }
 
     func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+        

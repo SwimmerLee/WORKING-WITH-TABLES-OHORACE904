e65b1dbc789855e0286644071a9dd6fe7282ccc5
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2023-02-24 21:49:37 +0000

    Fix update

--- 03_01-StockRank/StockRankUITests/StockRankUITests.swift
+++ 03_01-StockRank/StockRankUITests/StockRankUITests.swift
@@ -33,4 +33,4 @@ class StockRankUITests: XCTestCase {
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
-            meas
+            measure(metrics: [XCTAppli

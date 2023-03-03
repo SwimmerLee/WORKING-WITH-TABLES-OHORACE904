30bb43d3ef26f97172839c31f3e9497359846d37
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2023-03-03 14:07:27 +0000

    add name

--- 03_01-StockRank/StockRankUITests/StockRankUITests.swift
+++ 03_01-StockRank/StockRankUITests/StockRankUITests.swift
@@ -34,4 +34,5 @@ class StockRankUITests: XCTestCase {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication
+                XCUIApplication().launch()
+         

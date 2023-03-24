8f98e20bfe109984208e3ff45f916c0ccb8288d6
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2023-03-24 14:53:51 +0000

    Regen

--- 03_01-StockRank/StockRankUITests/StockRankUITestsLaunchTests.swift
+++ 03_01-StockRank/StockRankUITests/StockRankUITestsLaunchTests.swift
@@ -21,4 +21,4 @@ class StockRankUITestsLaunchTests: XCTestCase {
         let app = XCUIApplication()
         app.launch()
 
-        // Insert steps here to perform after app launc
+        // Insert steps here to perform after app launch but before taking a s

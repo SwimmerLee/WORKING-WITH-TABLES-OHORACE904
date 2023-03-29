dcbba3663d83aa4375265be1d55a10d49331af06
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2023-03-29 15:49:37 +0000

    Remove duplicated code.

--- 03_01-StockRank/StockRankUITests/StockRankUITestsLaunchTests.swift
+++ 03_01-StockRank/StockRankUITests/StockRankUITestsLaunchTests.swift
@@ -22,4 +22,4 @@ class StockRankUITestsLaunchTests: XCTestCase {
         app.launch()
 
         // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test a
+        // such as logging into a test account or navigating somewhe

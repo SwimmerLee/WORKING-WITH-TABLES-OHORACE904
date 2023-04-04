6cdaec90ed41746d27732a2a36ef407da1df99fd
SwimmerLee <108808426+SwimmerLee@users.noreply.github.com>
2023-04-04 20:29:50 +0000

    Regenerate .travis.yml

--- 03_01-StockRank/StockRankUITests/StockRankUITestsLaunchTests.swift
+++ 03_01-StockRank/StockRankUITests/StockRankUITestsLaunchTests.swift
@@ -27,4 +27,6 @@ class StockRankUITestsLaunchTests: XCTestCase {
         let attachment = XCTAttachment(screenshot: app.screenshot())
         attachment.name = "Launch Screen"
         attachment.lifetime = .keepAlways
-        add
+        add(attachment)
+    }
+}
